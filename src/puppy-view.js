d10ce2fdceb144f8aba9a9bc1cc8a1b53fb7e3e2
Alex T Leach <alextleach@gmail.com>
2016-08-01 16:08:20 -0500

    cant get it to update but it seems like it should

--- src/puppy-view.js
+++ src/puppy-view.js
@@ -1,5 +1,6 @@
 const cardTemplate = `
 <div class="puppy__card__image">
+          <form class="update-the-puppies">
           <img src="" class="puppy__card__image__img" alt="">
         </div>
         <div class="puppy__card__info">
@@ -20,12 +21,13 @@ const cardTemplate = `
 
           <div class="puppy__card__info__buttons">
             <div class="puppy__card__info__buttons--btn">
-              <button type="clear" class="puppy__button puppy__delete" name="button">DELETE</button>
+              <button type="button" class="puppy__button puppy__delete" name="button">DELETE</button>
             </div>
             <div class="puppy__card__info__buttons--btn">
-              <button type="submit" class="puppy__button puppy__update" name="button">PUPDATE</button>
+              <button type="submit" class="puppy__update puppy__button">PUPDATE</button>
             </div>
           </div>
+          </form>
         </div>
         `;
 
@@ -39,35 +41,60 @@ export default class PuppyView {
     this.element.innerHTML = cardTemplate;
 
     this.deletePuppy();
+    this.updatePuppy();
 
     this.render();
-    // this.element.appendChild(el);
   }
 
 
-
   render() {
-
-    this.element.querySelector('.dog__name').value= this.info.name;
+    this.element.querySelector('.dog__name').value = this.info.name;
     this.element.querySelector('.dog__age').value = this.info.age;
     this.element.querySelector('.photo__url').value = this.info.photoUrl;
     this.element.querySelector('.dog__description').value = this.info.profile;
     this.element.querySelector('.puppy__card__image__img').setAttribute('src', this.info.photoUrl);
   }
 
-  deletePuppy() {
-  this.element.querySelector('.puppy__delete').addEventListener('click', (ev) => {
-    ev.preventDefault();
 
-    fetch(`http://tiny-tn.herokuapp.com/collections/al-puppies/${this.info._id}`, {
-      method: `DELETE`,
-      body: JSON.stringify(this.element),
-    }).then((res) => res.json())
+  updatePuppy() {
+    this.element.querySelector('.update-the-puppies').addEventListener('submit', (ev) => {
+      ev.preventDefault();
+      fetch(`http://tiny-tn.herokuapp.com/collections/al-puppies/${this.info._id}`, {
+        method: 'PUT',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          _id: this.info._id,
+          name: this.element.querySelector('.dog__name').value,
+          age: this.element.querySelector('.dog__age').value,
+          url: this.element.querySelector('.photo__url').value,
+          profile: this.element.querySelector('.dog__description').value,
+        }),
+      }).then((res) => res.json())
+    .then((data) => {
+      this.info = data;
+
+      this.render();
+    });
+    });
+  }
+
+  deletePuppy() {
+    this.element.querySelector('.puppy__delete').addEventListener('click', (ev) => {
+      ev.preventDefault();
+
+      fetch(`http://tiny-tn.herokuapp.com/collections/al-puppies/${this.info._id}`, {
+        method: 'DELETE',
+        body: JSON.stringify(this.element),
+      }).then((res) => res.json())
   .then(() => {
     alert('BYE PUPPY!!!');
     this.app.removePuppyData(this.info);
+    this.render;
   });
-  });
-}
+    });
+  }
 
 }
